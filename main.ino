363c2bc3db6a03235de2c401fc948c5b2b827460
Luis Lucena Wanderley G. <66277412+luisfim@users.noreply.github.com>
2026-06-03 09:25:04 -0300

    Add Arduino sketch for keyboard input automation

---
+++ main.ino
@@ -0,0 +1,30 @@
+#include <Keyboard.h>
+
+const int buttonPin = 2;
+
+void setup() {
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  if (digitalRead(buttonPin) == LOW) {
+    while (1);
+  }
+
+  Keyboard.begin();
+  delay(3000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  delay(3000);
+  Keyboard.releaseAll();
+
+  Keyboard.println("notepad");
+  Keyboard.write(KEY_RETURN);
+  delay(3000);
+  Keyboard.println("Vc foi hackeado");
+
+  Keyboard.end();
+}
+
+void loop() {
+
+}
